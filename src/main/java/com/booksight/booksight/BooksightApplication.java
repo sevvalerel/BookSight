c9794b87090cfb568b5a3c102da4262484bd6b74
sevvalerel <160138147+sevvalerel@users.noreply.github.com>
2026-03-08 18:47:51 +0300

    CI pipeline oluşturuluyor

---
+++ src/main/java/com/booksight/booksight/BooksightApplication.java
@@ -0,0 +1,13 @@
+package com.booksight.booksight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booksight/booksight/BooksightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksight.booksight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
