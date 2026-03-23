283c27625861d8427d85ca3485c720400c69092f
sevvalerel <seyvalerel@gmail.com>
2026-03-23 22:17:47 +0300

    uçtan uca akış tamamlandı, JWT auth, öneri sistemi çalışıyor

--- src/main/java/com/booksight/booksight/entity/User.java
+++ src/main/java/com/booksight/booksight/entity/User.java
@@ -1,5 +1,6 @@
 package com.booksight.booksight.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
@@ -24,6 +25,7 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String passwordHash;
 
     @Column(nullable = false)
