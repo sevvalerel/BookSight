9795325055cec1d0addce3c33356a1e270a2a778
sevvalerel <seyvalerel@gmail.com>
2026-03-30 19:52:21 +0300

    SpringBootTest annotation kaldırıldı

--- src/test/java/com/booksight/booksight/BooksightApplicationTests.java
+++ src/test/java/com/booksight/booksight/BooksightApplicationTests.java
@@ -1,9 +1,7 @@
 package com.booksight.booksight;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BooksightApplicationTests {
 
 	@Test
